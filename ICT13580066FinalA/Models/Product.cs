a9130c661becfbd54f74daba01f6f4605e6f501d
pandapookpuii <nopompam_5566@hotmail.com>
2017-12-21 13:45:53 +0700

    final
    final

---
+++ ICT13580066FinalA/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SQLite;
+namespace ICT13580066FinalA.Models
+{
+    public class Product
+    {
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+
+		[NotNull]
+		[MaxLength(200)]
+		public string Name { get; set; }
+		public string Lasname { get; set; }
+		public string Address { get; set; }
+		public string Dpm { get; set; }
+
+		[NotNull]
+		[MaxLength(100)]
+
+		public string Gender { get; set; }
+		public string Email { get; set; }
+		public string Marry { get; set; }
+		public int Age { get; set; }
+		public int phone { get; set; }
+		public int Baby { get; set; }
+		public decimal Salary { get; set; }
+    }
+}
---
+++ ICT13580066FinalA/ProductNewPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ICT13580066FinalA
+{
+    public partial class ProductNewPage : ContentPage
+    {
+        public ProductNewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
